c8597fbae9f4f89cedee37c9d26280a1a62abd69
Jiyun Park <51026374+PrimWILL@users.noreply.github.com>
2022-09-21 22:12:11 +0900

    Delete 29_Jewels_and_Stones.py

--- 10장 데크, 우선순위 큐/29_Jewels_and_Stones.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        count = collections.Counter(stones)
-        result = 0
-        
-        for jewel in jewels:
-            result += count[jewel]
-        return result
